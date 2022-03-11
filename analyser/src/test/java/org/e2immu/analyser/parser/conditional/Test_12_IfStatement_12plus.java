042198ba7078b3ff1d7bfb4ecd584923ab165ae9
Bart Naudts <bart.naudts@ibtech.be>
2022-03-11 17:20:50 +0100

    extra test for unexpected error; does not catch the one I'm looking for

---
+++ analyser/src/test/java/org/e2immu/analyser/parser/conditional/Test_12_IfStatement_12plus.java
@@ -0,0 +1,60 @@
+/*
+ * e2immu: a static code analyser for effective and eventual immutability
+ * Copyright 2020-2021, Bart Naudts, https://www.e2immu.org
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details. You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.e2immu.analyser.parser.conditional;
+
+import org.e2immu.analyser.analyser.DV;
+import org.e2immu.analyser.analyser.EvaluationResult;
+import org.e2immu.analyser.analyser.Property;
+import org.e2immu.analyser.config.AnalyserConfiguration;
+import org.e2immu.analyser.config.DebugConfiguration;
+import org.e2immu.analyser.model.MultiLevel;
+import org.e2immu.analyser.model.ParameterInfo;
+import org.e2immu.analyser.model.expression.InlinedMethod;
+import org.e2immu.analyser.model.variable.FieldReference;
+import org.e2immu.analyser.model.variable.ReturnVariable;
+import org.e2immu.analyser.model.variable.This;
+import org.e2immu.analyser.model.variable.VariableNature;
+import org.e2immu.analyser.parser.CommonTestRunner;
+import org.e2immu.analyser.visitor.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test_12_IfStatement_12plus extends CommonTestRunner {
+    public Test_12_IfStatement_12plus() {
+        super(true);
+    }
+
+    // simpler version of DependentVariables_3, without the delays
+    @Test
+    public void test_12() throws IOException {
+        StatementAnalyserVariableVisitor statementAnalyserVariableVisitor = d -> {
+            if ("method".equals(d.methodInfo().name)) {
+
+            }
+        };
+        StatementAnalyserVisitor statementAnalyserVisitor = d -> {
+
+        };
+        testClass("IfStatement_12", 0, 0, new DebugConfiguration.Builder()
+                .addStatementAnalyserVariableVisitor(statementAnalyserVariableVisitor)
+                .addStatementAnalyserVisitor(statementAnalyserVisitor)
+                .build(), new AnalyserConfiguration.Builder().setForceAlphabeticAnalysisInPrimaryType(true).build());
+    }
+}
---
+++ analyser/src/test/java/org/e2immu/analyser/parser/conditional/testexample/IfStatement_12.java
@@ -0,0 +1,78 @@
+/*
+ * e2immu: a static code analyser for effective and eventual immutability
+ * Copyright 2020-2021, Bart Naudts, https://www.e2immu.org
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details. You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.e2immu.analyser.parser.conditional.testexample;
+
+import java.util.List;
+import java.util.Set;
+
+public class IfStatement_12 {
+
+    static class ParameterizedType {
+        private final String s;
+
+        public ParameterizedType(String in) {
+            s = in;
+        }
+
+        boolean isLong() {
+            return s == null || s.startsWith("x");
+        }
+
+        boolean isInt() {
+            return s != null && s.endsWith("i");
+        }
+    }
+
+    static class MethodInspection {
+        private boolean b;
+
+        public void setB(boolean b) {
+            this.b = b;
+        }
+
+        List<ParameterInfo> getParameters() {
+            return b ? List.of(new ParameterInfo(new ParameterizedType("i"), 0)) : List.of();
+        }
+    }
+
+    interface InspectionProvider {
+        MethodInspection getMethodInspection(IfStatement_12 ifStatement_12);
+    }
+
+    record ParameterInfo(ParameterizedType parameterizedType, int index) {
+    }
+
+    private final String name;
+
+    public IfStatement_12(String name) {
+        this.name = name;
+    }
+
+    private static final Set<String> ZERO_PARAMS = Set.of("toString", "hashCode", "clone", "finalize", "getClass",
+            "notify", "notifyAll", "wait");
+
+    public boolean method(InspectionProvider inspectionProvider) {
+        List<ParameterInfo> parameters = inspectionProvider.getMethodInspection(this).getParameters();
+        int numParameters = parameters.size();
+        if (numParameters == 0) {
+            return ZERO_PARAMS.contains(name);
+        }
+        if (numParameters == 1) {
+            return "equals".equals(name) || "wait".equals(name) && parameters.get(0).parameterizedType.isLong();
+        }
+        return numParameters == 2 && "wait".equals(name) && parameters.get(0).parameterizedType.isLong() &&
+                parameters.get(1).parameterizedType.isInt();
+    }
+}
