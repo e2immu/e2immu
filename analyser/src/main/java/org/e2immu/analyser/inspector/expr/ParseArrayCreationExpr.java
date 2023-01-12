ddade2b9bd7d619f7903918604add8f60b9a8d41
Bart Naudts <bart.naudts@ibtech.be>
2023-01-12 21:48:23 +0100

    made method public

--- analyser/src/main/java/org/e2immu/analyser/inspector/expr/ParseArrayCreationExpr.java
+++ analyser/src/main/java/org/e2immu/analyser/inspector/expr/ParseArrayCreationExpr.java
@@ -50,7 +50,7 @@ public class ParseArrayCreationExpr {
     }
 
     // new Type[3]; this method creates the constructor that makes this array, without attaching said constructor to the type
-    static MethodInfo createArrayCreationConstructor(TypeContext typeContext, ParameterizedType parameterizedType) {
+    public static MethodInfo createArrayCreationConstructor(TypeContext typeContext, ParameterizedType parameterizedType) {
         MethodInspection.Builder builder = new MethodInspectionImpl.Builder(parameterizedType.typeInfo)
                 .setInspectedBlock(Block.emptyBlock(Identifier.generate("empty block in array creation")))
                 .setReturnType(parameterizedType)
